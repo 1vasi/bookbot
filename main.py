b4e98a4435fafb0a3510b1b88a6b3a20d6a6f687
1vasi <yungvasi1@gmail.com>
2025-07-11 10:45:09 +0300

    no comment

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
+import sys
 from stats import get_num_words
+from stats import get_num_letter
+from stats import sort_dict
+
+
+import sys
+
+try:
+    text_file = sys.argv[1]  # Try to access the argument
+except IndexError:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    get_book_text("/Users/yungvasi/projects/github.com/1vasi/bookbot/books/frankenstein.txt")
 
-    print(get_num_words(get_book_text("/Users/yungvasi/projects/github.com/1vasi/bookbot/books/frankenstein.txt")))
+    
+
+    get_book_text(text_file)
+    
+    get_num_words(get_book_text(text_file))
+
+    get_num_letter(get_book_text(text_file))
+
+    sort_dict(get_num_letter(get_book_text(text_file)))
 
 def get_book_text(filepath):
     with open(filepath) as f:
         output_str = f.read()
         return output_str
+
+def print_report():
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/{text_file}")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_book_text(text_file))} total words")
+    print("--------- Character Count -------")
+    for i in sort_dict(get_num_letter(get_book_text(text_file))):
+        print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
+
+print_report()
+
+
+main() 
+
+
     
-main()
+
 
--- stats.py
+++ stats.py
@@ -3,4 +3,27 @@ def get_num_words(str1):
     lst= str1.split()
     for word in lst:
         num_words += 1
-    return f"{num_words} words found in the document"
+    return num_words
+
+def get_num_letter(str1):
+    dict_letter = {}
+    list_of_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        
+    for i in list_of_letters:
+        correct_str = str1.lower()
+        each_letter_counted = correct_str.count(i)
+        dict_letter[i] = each_letter_counted
+        
+    return dict_letter
+    
+def sort_dict(lst_dict):
+    lst = []
+
+    for k,v in lst_dict.items():
+        new_dict = {"char": k, "num": v}
+        lst.append(new_dict)  
+    lst.sort(reverse=True, key=lambda x: x['num'])
+
+    return lst
+
